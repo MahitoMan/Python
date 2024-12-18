de6b5b666534bb51a26756fda1bfa682e0e41051
MahitoMan <st089270@student.spbu.ru>
2024-12-18 16:41:05 +0300

    lll

--- 2024-pscr/task11/task11.py
+++ 2024-pscr/task11/task11.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 def get_top_trimers(sequence):
     # Создаем список всех возможных тримеров
-    trimers = [sequence[i:i+3] for i in range(len(sequence)-2)]
+    trimers = [sequence[i:i + 3] for i in range(len(sequence) - 2)]
     
     # Считаем количество каждого тримера
     counter = Counter(trimers)
@@ -15,7 +15,6 @@ def get_top_trimers(sequence):
 # Основная логика программы
 sequences = []
 current_sequence = ""
-empty_lines_count = 0
 
 while True:
     try:
@@ -23,22 +22,19 @@ while True:
     except EOFError:
         break
     if not line.strip():
-        empty_lines_count += 1
-        if empty_lines_count == 2:
-            sequences.append(current_sequence)
-            break
-        else:
-            sequences.append(current_sequence)
-            current_sequence = ""
-    elif line.startswith(">"):
+        continue
+    elif line.startswith('>'):
         if current_sequence:
             sequences.append(current_sequence)
         current_sequence = ""
     else:
         current_sequence += line.strip().upper()
 
+if current_sequence:
+    sequences.append(current_sequence)
+
 for sequence in sequences:
     top_4 = get_top_trimers(sequence)
-    print(f'Для последовательности {sequence}:')
+    print(f'\nДля последовательности {sequence}:')
     for trimer, count in top_4:
         print(f'{trimer} встречается {count} раз.')
